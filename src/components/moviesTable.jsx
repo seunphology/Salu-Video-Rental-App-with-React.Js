1594a2d05edfb7a85e00b5bc7946d48d9f6e77c1
seunphology <seunsalu87@gmail.com>
2022-09-04 02:01:15 +0200

    grant admin access to features

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import auth from '../services/authService'; // this is imported because only authentcated users must have access to certain features on the table
 import Table from './common/table';
 import { Link } from 'react-router-dom';
 
@@ -16,15 +17,27 @@ class MoviesTable extends Component {
         {path: 'genre.name', label: 'Genre'},
         {path: 'numberInStock', label: 'Stock'},
         {path: 'dailyRentalRate', label: 'Rate'},
-        {key:"like", content: movie => <Like liked={movie.liked} onClick= {() =>this.props.onLike(movie)} />}, // for like
-        {key:"delete", content: movie=> (<button onClick={() => this.props.onDelete(movie)} className= "btn btn-danger btn-sm" >Delete</button>)} // for delete
+        {key:"like", content: movie => (<Like liked={movie.liked} onClick= {() =>this.props.onLike(movie)} />)} // for like
+        
         
 
 
     ];  
 
+    // To delete....which is to be done only by admin
+deleteColumn = {
+    key:"delete", content: movie=> (<button onClick={() => this.props.onDelete(movie)} className= "btn btn-danger btn-sm" >Delete</button>)} ;
 
 
+
+
+constructor () {
+
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin)
+    this.columns.push(this.deleteColumn);
+};
     
     render() { 
 
@@ -32,8 +45,8 @@ class MoviesTable extends Component {
 
         const {movies,  onSort, sortColumn} = this.props;
     return ( <Table columns={this.columns} data={movies} sortColumn= {sortColumn} onSort={onSort} />);
-    }
-}
+    };
+};
  
  
 export default MoviesTable ;
